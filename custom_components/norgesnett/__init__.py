6663204c8e462d841601b159c4665aba89bb91b0
Quint1337 <36964445+MAFL74@users.noreply.github.com>
2025-06-14 11:37:02 +0200

    Update __init__.py

--- custom_components/norgesnett/__init__.py
+++ custom_components/norgesnett/__init__.py
@@ -14,7 +14,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
